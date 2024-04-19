ce7b2851511b02be1a5f99fa229a1cebfb664fef
twingoose <140074426+twingoose@users.noreply.github.com>
2024-04-19 10:11:49 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,70 @@
+x = 0;
+y = 0;
+
+draw_apple = "";
+
+var SpeechRecognition = window.webkitSpeechRecognition;
+  
+var recognition = new SpeechRecognition();
+
+function start()
+{
+  document.getElementById("status").innerHTML = "System is listening please speak";  
+  recognition.start();
+} 
+ 
+recognition.onresult = function(event) {
+
+ console.log(event); 
+
+ content = event.results[0][0].transcript;
+
+    document.getElementById("status").innerHTML = "The speech has been recognized: " + content; 
+
+}
+
+function setup() {
+ 
+}
+
+function draw() {
+  if(draw_apple == "set")
+  {
+    document.getElementById("status").innerHTML = to_number + " Apples drawn";
+    draw_apple = "";
+  }
+}
+
+function speak(){
+    var synth = window.speechSynthesis;
+
+    var utterThis = new SpeechSynthesisUtterance(speak_data);
+
+    synth.speak(utterThis);
+
+    speak_data = "";
+}
+
+
+var screen_width = [0];
+
+
+
+var screen_height = [0];
+
+
+
+var apple = empty;
+
+var speak_data = empty;
+
+
+var  to_number = empty;
+ to_number = Number(content);
+if(Number.isInteger(to_number))
+if(true document.getElementById)
+
+function preload(){
+  apple = loadImage("apple.png");
+  }
+  
